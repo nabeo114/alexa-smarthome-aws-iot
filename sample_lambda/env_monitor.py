4945e10d4348c43792e417a16be83aa355231fd8
nabeo114 <nabeo114@icloud.com>
2021-01-05 13:35:22 +0900

    Update Env monitor

--- sample_lambda/env_monitor.py
+++ sample_lambda/env_monitor.py
@@ -6,12 +6,28 @@ import json
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-def get_env_temperature():
-    dynamodb = boto3.resource('dynamodb', region_name=os.environ['Region'])
-    table = dynamodb.Table(os.environ['TableName'])
-    
-    response = table.get_item(Key = {os.environ['PartitionKey']: os.environ['PartitionName']})
-    logger.info(response)
-    temperature = response["Item"]["temperature"]
-    
-    return float(temperature)
+class EnvMonitor:
+    def __init__(self):
+        self.dynamodb = boto3.resource('dynamodb', region_name=os.environ['Region'])
+        self.table = self.dynamodb.Table(os.environ['TableName'])
+
+        self.update_data()
+
+    def get_temperature(self):
+        self.update_data()
+        return float(self.temperature)
+
+    def get_humidity(self):
+        self.update_data()
+        return float(self.humidity)
+
+    def get_pressure(self):
+        self.update_data()
+        return float(self.pressure)
+
+    def update_data(self):
+        response = self.table.get_item(Key = {os.environ['PartitionKey']: os.environ['PartitionName']})
+        logger.info(response)
+        self.temperature = response["Item"]["temperature"]
+        self.humidity = response["Item"]["humidity"]
+        self.pressure = response["Item"]["pressure"]
